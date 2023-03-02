d0f6e5b1fc73d1e0263444fb9ee8c6e6d69785e0
usernamemu <mhmdarif.developer@gmail.com>
2023-03-02 11:49:19 +0700

    feat(device): add device controller, model, routes

---
+++ app/Http/Controllers/Api/DeviceController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Device;
+use Illuminate\Http\Request;
+
+class DeviceController extends Controller
+{
+    public function index()
+    {
+        $devices = Device::paginate();
+        return response()->json($devices);
+    }
+
+    public function store(Request $request)
+    {
+        $request -> validate([
+            'device_name' => 'required',
+            'kwh' => 'required'
+        ]);
+
+        // $device = Device::create([
+        //     'device_name' => $request->device_name,
+        //     'kwh' => $request->kwh      
+        // ]);
+
+        $device = Device::create($request->all());
+        return response()->json($device);
+    }
+
+    public function show(Device $device)
+    {
+        return $device;
+    }
+
+    public function update(Device $device, Request $request)
+    {
+        $request -> validate([
+            'device_name' => 'required',
+            'kwh' => 'required'
+        ]);
+
+        $device->device_name = $request->device_name;
+        $device->kwh = $request->kwh;
+        $device->save();
+
+        return response()->json($device);
+    }
+
+    public function destroy(Device $device)
+    {
+        $device->delete();
+
+        return response()-> json();
+    }
+}
+
