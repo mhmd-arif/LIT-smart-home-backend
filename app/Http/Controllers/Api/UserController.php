0b687084244347540ea11e5cefe62c700df119f2
usernamemu <mhmdarif.developer@gmail.com>
2023-03-17 09:19:36 +0700

    feat: add users crud method

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,5 +8,30 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+    public function index()
+    {
+        $users = User::paginate();
+        return response()->json($users);
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $request -> validate([
+            'name' => 'required',
+            'email' => 'required',
+        ]);
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        return response()->json($user);
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response()-> json(['message'=>'user deleted successfully']);
+    }
 }
