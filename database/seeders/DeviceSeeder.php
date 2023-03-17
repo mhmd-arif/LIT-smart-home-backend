c7a0c97fa2e86b7a74148ec14b30e03c53840d83
saddansyah <saddanakbar@gmail.com>
2023-03-17 19:58:45 +0700

    feat: add device properties (icon and is_favorite)

--- database/seeders/DeviceSeeder.php
+++ database/seeders/DeviceSeeder.php
@@ -14,22 +14,43 @@ class DeviceSeeder extends Seeder
     public function run(): void
     {
         DB::table('devices')->insert([
-            ["user_id" => 1,
+            [
+                "user_id" => 1,
                 "device_name" => "LIT-Lamp-1",
                 "category" => "Smart Lamp",
+                "is_favorite" => true,
                 "volt" => "100",
                 "ampere" => "1",
                 "watt" => "100",
-                "state" => 1,
+                "state" => true,
+                "icon_url" => "mdi-ceiling-light",
+                "created_at" => now(),
                 "updated_at" => now(),
             ],
-            ["user_id" => 2,
+            [
+                "user_id" => 2,
                 "device_name" => "LIT-Lamp-2",
                 "category" => "Smart Lamp",
+                "is_favorite" => false,
                 "volt" => "200",
                 "ampere" => "1",
                 "watt" => "200",
-                "state" => 0,
+                "state" => false,
+                "icon_url" => "mdi-ceiling-light",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "user_id" => 2,
+                "device_name" => "LIT-Super-Duper-Smart-Terminal",
+                "category" => "Smart Terminal",
+                "is_favorite" => true,
+                "volt" => "100",
+                "ampere" => "1",
+                "watt" => "120",
+                "state" => false,
+                "icon_url" => "mdi-power-socket-eu",
+                "created_at" => now(),
                 "updated_at" => now(),
             ]
         ]);
