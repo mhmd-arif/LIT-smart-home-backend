a09532d251fbebf0c6c732b09f4aa2240e51b77d
usernamemu <mhmdarif.developer@gmail.com>
2023-03-22 22:08:52 +0700

    feat(elo relation): add elo relation all model

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -4,11 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\DeviceUsage;
+use App\Models\Device;
 
 class Device extends Model
 {
     use HasFactory;
 
     public $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function deviceUsage(): HasMany
+    {
+        return $this->hasMany(DeviceUsage::class);
+    }
 
 }
