4075d162e998b3b10ddfa13c63267457aa0505fc
usernamemu <mhmdarif.developer@gmail.com>
2023-03-30 22:03:15 +0700

    refactor(routes): shorting route

--- routes/api.php
+++ routes/api.php
@@ -28,34 +28,34 @@ Route::post('auth/register', [AuthController::class, 'register']);
 Route::post('auth/login', [AuthController::class, 'login']);
 
 // device usage automation
-Route::post('device_usages/create', [DeviceUsageController::class, 'createUsage']);
+Route::post('device_usages', [DeviceUsageController::class, 'createUsage']);
 
 // device
-Route::post('devices/create', [DeviceController::class, 'createDevice']);
+Route::post('devices', [DeviceController::class, 'createDevice']);
 Route::get('devices', [DeviceController::class, 'getDevices']);
 Route::get('devices/{id}', [DeviceController::class, 'findDevice']);
-Route::put('devices/update_device/{id}', [DeviceController::class, 'updateDevice']);
-Route::delete('devices/delete/{id}', [DeviceController::class, 'deleteDevices']);
+Route::put('devices/{id}', [DeviceController::class, 'updateDevice']);
+Route::delete('devices/{id}', [DeviceController::class, 'deleteDevices']);
 
 //Protecting Routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
     // User device
-    Route::post('user_devices/create', [UserDeviceController::class, 'createUserDevice']);
+    Route::post('user_devices', [UserDeviceController::class, 'createUserDevice']);
     Route::get('user_devices', [UserDeviceController::class, 'getUserDevices']);
     Route::get('user_devices/{id}', [UserDeviceController::class, 'findUserDevice']);
-    Route::put('user_devices/update_device/{id}', [UserDeviceController::class, 'updateUserDevice']);
+    Route::put('user_devices/{id}', [UserDeviceController::class, 'updateUserDevice']);
     Route::patch('user_devices/update_state/{id}', [UserDeviceController::class, 'updateState']);
     Route::patch('user_devices/update_favorite/{id}', [UserDeviceController::class, 'updateFavorite']);
-    Route::delete('user_devices/delete/{id}', [UserDeviceController::class, 'deleteUserDevices']);
+    Route::delete('user_devices/{id}', [UserDeviceController::class, 'deleteUserDevices']);
 
     // device usage per device
     Route::get('device_usages', [DeviceUsageController::class, 'getUsages']);
-    Route::get('device_usages/get_usage/{id}', [DeviceUsageController::class, 'findUsage']);
+    Route::get('device_usages/{id}', [DeviceUsageController::class, 'findUsage']);
 
     // usages total all device
     Route::get('total_usages', [TotalUsageController::class, 'getTotalUsage']);
-    Route::get('total_usages/hourly/', [TotalUsageController::class, 'getTotalUsageHourly']);
+    Route::get('total_usages/hourly', [TotalUsageController::class, 'getTotalUsageHourly']);
     Route::get('total_usages/daily', [TotalUsageController::class, 'getTotalUsageDaily']);
     Route::get('total_usages/weekly', [TotalUsageController::class, 'getTotalUsageWeekly']);
     Route::get('total_usages/monthly', [TotalUsageController::class, 'getTotalUsageMonthly']);
@@ -66,4 +66,6 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
     // logout
     Route::post('auth/logout', [AuthController::class, 'logout']);
+
+    
 });
